11a68946b191d93b948693e6519024e1da84ab9f
ari-bag <abaggerman@emergya.com>
2021-11-23 19:04:10 +0100

    solve cors

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 // require express and bodyParser
 const  express = require("express");
 const  bodyParser = require("body-parser");
-const cors = require('cors');
 
 require("./config/db")
 
@@ -12,6 +11,27 @@ var routes = require('./routes/api'); //importing route
 
 // create express app
 const  app = express();
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 
 app.use("/api", routes);
 
@@ -22,19 +42,6 @@ const  port = process.env.PORT || 3000;
 // use bodyParser middleware on express app
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", '*');
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
-
-app.use(cors());
-
-app.use(cors({ origin: "https://shrouded-castle-77871.herokuapp.com/", credentials: true }))
 
 // Add endpoint
 app.get('/', (req, res) => {
