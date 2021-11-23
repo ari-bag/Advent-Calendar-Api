8b2887d30d5c1875a31d3da7cf9e5911eb584ac3
ari-bag <abaggerman@emergya.com>
2021-11-23 18:51:01 +0100

    solve cors

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ const  port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+
 app.use(cors());
 
 app.use(cors({ origin: "https://shrouded-castle-77871.herokuapp.com/", credentials: true }))
